49429bb2d3f5da964fb325638166d04588f2e005
kingd <dkinguag@gmail.com>
2019-10-21 19:22:18 -0500

    Getting a weird error, gatsby develop fail

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,9 +6,15 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
+import BuyButton from '../components/BuyButton'
 
 class BlogPostTemplate extends React.Component {
   render() {
+    const images = post.frontmatter.image
+  .map(x => ({
+    name: x.name,
+    src: require(`./../../content/assets${post.frontmatter.path}${x.src}.jpg`)
+  }));
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
@@ -91,22 +97,26 @@ class BlogPostTemplate extends React.Component {
 export default BlogPostTemplate
 
 export const pageQuery = graphql`
-  query BlogPostBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-        author
+      query BlogPostBySlug($slug: String!) {
+        site {
+          siteMetadata {
+            title
+            author
+          }
+        }
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+          id
+          excerpt
+          html
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            price
+            id
+            path
+            description
+            image
+          }
+        }
       }
-    }
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        description
-      }
-    }
-  }
-`
+    `
