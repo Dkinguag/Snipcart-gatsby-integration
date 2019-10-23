10bda010b059d4321fe6296ccea885e9ad90a067
kingd <dkinguag@gmail.com>
2019-10-22 22:07:01 -0500

    Step 3 Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,6 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
-        <Bio />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
@@ -38,10 +37,12 @@ class BlogIndex extends React.Component {
                     __html: node.frontmatter.description || node.excerpt,
                   }}
                 />
+
               </section>
             </article>
           )
         })}
+        <Bio/>
       </Layout>
     )
   }
